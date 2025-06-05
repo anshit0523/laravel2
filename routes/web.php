827f2ce4b5de0d3045a91ab54372626c48b14285
anshit0523 <fernnadosaranajr@gmail.com>
2025-06-05 16:26:29 +0800

    act2 - create

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
 Route::get('/', [CarController::class, 'index'])->name('cars.index');
+
+Route::get('/cars/create', [CarController::class, 'create'])->name('cars.create');
+Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
